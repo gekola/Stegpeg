b3aadfcd09f4cf16e352cec730dc956baa637225
Nick Herman <gerkola@gmail.com>
2015-04-30 23:55:08 +0300

    Fix tests warnings

--- tests/lsb.rs
+++ tests/lsb.rs
@@ -3,18 +3,17 @@ extern crate stegpeg;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
-use std::io::Write;
 use std::path::Path;
 
 use stegpeg::algorythms::lsb;
 
-const orig_img_path: &'static str = "tests/files/lena.jpg";
-const secret_path:   &'static str = "tests/files/test.txt";
-const out_img_path:  &'static str = "/tmp/out.jpg";
+const ORIG_IMG_PATH: &'static str = "tests/files/lena.jpg";
+const SECRET_PATH:   &'static str = "tests/files/test.txt";
+const OUT_IMG_PATH:  &'static str = "/tmp/out.jpg";
 
 #[test]
 fn test_lsb() {
-  let path = Path::new(secret_path);
+  let path = Path::new(SECRET_PATH);
   let mut file = match File::open(&path) {
     Ok(file) => file,
     Err(err) => panic!("couldn't open {}: {}", path.display(),
@@ -25,14 +24,14 @@ fn test_lsb() {
 
   match file.read_to_end(&mut data) {
     Ok(_) => {
-      stegpeg::encode_file(orig_img_path, out_img_path, &|coefs| {
+      stegpeg::encode_file(ORIG_IMG_PATH, OUT_IMG_PATH, &|coefs| {
         return lsb::enc(coefs, &data);
       });
     }
     Err(err) => panic!("couldn't write: {}", err)
   }
 
-  let coefs = stegpeg::decode_file(out_img_path);
+  let coefs = stegpeg::decode_file(OUT_IMG_PATH);
   let new_data = match stegpeg::algorythms::lsb::dec(&coefs) {
     Ok(new_data) => new_data,
     Err(err) => panic!("{}", err)
